f807f82f685f5f4f3406c77a1877770bfe77c655
Alexandre Francisco <142149179+ALEXANDREFRANCISCO-ID@users.noreply.github.com>
2024-01-03 18:32:46 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,21 @@
+$(document).ready(function() {
+    var emails = ["email1@example.com", "email2@example.com"]; // Insira aqui os emails válidos
+    var codigos = ["codigo1", "codigo2"]; // Insira aqui os códigos correspondentes
 
+    $("#loginForm").submit(function(event) {
+        event.preventDefault();
+        var email = $("#emailInput").val();
+        var senha = $("#passwordInput").val();
+
+        // Verifica se o email e a senha correspondem
+        var index = emails.indexOf(email);
+        if (index !== -1 && codigos[index] === senha) {
+            // Redirecionar para a página principal
+            window.location.href = "pagina_principal.html";
+        } else {
+            // Exibir mensagem de erro
+            $("#message").text("Email ou senha incorretos");
+        }
+    });
+});
+ 
